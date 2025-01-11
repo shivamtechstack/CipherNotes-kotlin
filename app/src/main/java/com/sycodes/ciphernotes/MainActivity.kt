be4f1eda437fdd9433730aff0228ff2ee350d6d1
shivamtechstack <devshivamyadav1604@gmail.com>
2025-01-11 13:26:09 +0530

    -SignIn & SignUp page designed

--- app/src/main/java/com/sycodes/ciphernotes/MainActivity.kt
+++ app/src/main/java/com/sycodes/ciphernotes/MainActivity.kt
@@ -5,8 +5,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.FragmentContainerView
+import com.sycodes.ciphernotes.fragments.SignInFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var fragmentContainer: FragmentContainerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +19,8 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        fragmentContainer = findViewById(R.id.loginSignupfragmentContainerView)
+
+        supportFragmentManager.beginTransaction().replace(R.id.loginSignupfragmentContainerView, SignInFragment()).commit()
     }
 }
