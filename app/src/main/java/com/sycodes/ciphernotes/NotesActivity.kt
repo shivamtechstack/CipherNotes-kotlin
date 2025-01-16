f7ff1a5e597f444b4b3a7356e406e8dcb7dcb338
shivamtechstack <devshivamyadav1604@gmail.com>
2025-01-16 14:06:00 +0530

    -Added Notes Home and Notes Writing fragments with basic layouts -Implemented navigation drawer in Notes Home fragment -Integrated Floating Action Button for adding notes in Notes Home fragment -Added dependencies for Firebase database

--- app/src/main/java/com/sycodes/ciphernotes/NotesActivity.kt
+++ app/src/main/java/com/sycodes/ciphernotes/NotesActivity.kt
@@ -5,16 +5,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.sycodes.ciphernotes.fragments.NotesHomeFragment
 
 class NotesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_notes)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.notesFragmentContainer)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        supportFragmentManager.beginTransaction().replace(R.id.notesFragmentContainer, NotesHomeFragment()).commit()
     }
 }
---
+++ app/src/main/java/com/sycodes/ciphernotes/fragments/NotesHomeFragment.kt
@@ -0,0 +1,52 @@
+package com.sycodes.ciphernotes.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.ActionBarDrawerToggle
+import com.sycodes.ciphernotes.R
+import com.sycodes.ciphernotes.databinding.FragmentNotesHomeBinding
+
+
+class NotesHomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentNotesHomeBinding
+    private lateinit var toggle: ActionBarDrawerToggle
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentNotesHomeBinding.inflate(inflater, container, false)
+        drawerSetup()
+
+        return binding.root
+    }
+    private fun drawerSetup(){
+        toggle = ActionBarDrawerToggle(
+            requireActivity(),
+            binding.drawerlayout,
+            binding.notesToolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        binding.drawerlayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        // Handle Navigation Item Selection
+        binding.navigationView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+
+            }
+            binding.drawerlayout.closeDrawers()
+            true
+        }
+    }
+}
