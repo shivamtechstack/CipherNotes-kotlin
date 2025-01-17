4dcf6b7f12bb6858c237a74d339e029f46cc490c
shivamtechstack <devshivamyadav1604@gmail.com>
2025-01-17 17:35:23 +0530

    -Room database implementation with Note entity, DAO, and database class; added Firestore dependency.

--- app/src/main/java/com/sycodes/ciphernotes/data/Note.kt
+++ app/src/main/java/com/sycodes/ciphernotes/data/Note.kt
@@ -1,5 +1,16 @@
 package com.sycodes.ciphernotes.data
 
-data class Note(
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
+@Entity(tableName = "notes_table")
+data class Note(
+    @PrimaryKey(autoGenerate = false)
+    val id: String = "",
+    val title: String = "",
+    val content: String = "",
+    val dateCreated: Int = 0,
+    val lastModified: Int = 0,
+    val isPinned: Boolean = false,
+    val isLocked: Boolean = false,
 )
--- app/src/main/java/com/sycodes/ciphernotes/room/NoteDao.kt
+++ app/src/main/java/com/sycodes/ciphernotes/room/NoteDao.kt
@@ -1,4 +1,25 @@
 package com.sycodes.ciphernotes.room
 
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.sycodes.ciphernotes.data.Note
+
+@Dao
 interface NoteDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertNote(note: Note)
+
+    @Delete
+    suspend fun deleteNote(note: Note)
+
+    @Query("SELECT * FROM notes_table ORDER BY lastModified DESC")
+    fun getAllNotes(): LiveData<List<Note>>
+
+    @Query("SELECT * FROM notes_table WHERE id = :id")
+    fun getNoteById(id: String): LiveData<Note>
+
 }
